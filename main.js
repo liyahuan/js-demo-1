fe8e9d70df25176e17533e1abe72347eebd59e09
liyahuan <liyahuan@aragoncs.com>
2020-12-24 17:28:33 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+var Div = document.createElement('div');
+
+Div.className='demo';
+
+document.body.appendChild(Div);
+
+var dragging = false;
+
+var lastX;
+
+var lastY;
+
+Div.onmousedown = function (e) {
+    console.log('原始位置');
+    lastX=e.clientX;
+    lastY=e.clientY;
+    dragging=true;
+}
+
+document.onmousemove = function (e) {
+
+    if(dragging===true){
+
+        var deltaX=e.clientX -lastX;
+
+        var deltaY=e.clientY -lastY;
+
+        var top = parseInt(Div.style.top) || 0;
+
+        var left = parseInt(Div.style.left) || 0;
+
+        var resultX = top + deltaY;
+
+        var resultY = left + deltaX;
+
+        if(resultX<0){
+            resultX=0;
+        }
+        if(resultY<0){
+
+            resultY=0;
+        }
+
+        Div.style.top = resultX + 'px';
+
+        Div.style.left = resultY + 'px';
+
+        lastX = e.clientX;
+
+        lastY = e.clientY;
+
+        console.log('Div style top');
+
+        console.log(Div.style.top);
+
+
+    }
+}
+
+document.onmouseup=function (ev) {
+    dragging=false;
+}
